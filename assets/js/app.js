f5f27a59a34707f4db5ba445e1d0de32e495b2bf
Sathvik Aithala <sathvik@Sathviks-MacBook-Pro.local>
2020-11-03 13:58:12 -0600

    housing blog post done, plus some other minor updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,21 @@ function showImgContent(e) {
 
 document.addEventListener('mousemove', showImgContent);
 
+/* tableau dashboard embed: */
+var divElement = document.getElementById('db'); 
+var vizElement = divElement.getElementsByTagName('object')[0]; 
+if (divElement.offsetWidth > 800) { 
+    vizElement.style.width = '1000px'; 
+    vizElement.style.height = '800px'; 
+    } 
+else if (divElement.offsetWidth > 500) { 
+    vizElement.style.width = '1000px'; 
+    vizElement.style.height = '800px'; 
+    } 
+else { 
+    vizElement.style.width = '100%'; 
+    vizElement.style.height = '1400px'; 
+    } 
+var scriptElement = document.createElement('script'); 
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; 
+vizElement.parentNode.insertBefore(scriptElement, vizElement); 
